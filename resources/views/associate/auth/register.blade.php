77e9f3ca8b6b4ccb0ac8b67a0304df7f1d32c1de
marochan <iwata26th0776heartasone@gmail.com>
2022-07-13 18:21:29 +0900

    no message

--- resources/views/associate/auth/register.blade.php
+++ resources/views/associate/auth/register.blade.php
@@ -94,7 +94,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('パスワード確認') }}</label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('パスワード（確認）') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
