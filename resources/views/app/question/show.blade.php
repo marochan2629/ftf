1d8aa33762d577b9428e2bd972d4e2dddc7828e8
marochan <iwata26th0776heartasone@gmail.com>
2022-07-30 16:09:29 +0900

    質問詳細画面一旦完成

--- resources/views/app/question/show.blade.php
+++ resources/views/app/question/show.blade.php
@@ -15,22 +15,19 @@
                         </div>
                     </div>
                     
-
-                    <div class="form-group row">
-                       <p class="col-sm-2 col-form-label">回答</p>
-                       <div class="col-sm-10">
-                           <div>{!! nl2br(e($question->answer)) !!}</div>
-                       </div>
-                   </div>
-           
-                   @if($question->sup_image)
-                       <div class="form-group row">
-                           <p class="col-sm-2 col-form-label">画像</p>
-                           <div class="col-sm-10">
-                               <img src="{{ Storage::url($question->sup_image) }}" width="25%">
-                           </div>
-                       </div>
-                   @endif
+                    <div class="question-show-answer-wrapper">
+                        <h2>Answer</h2>
+                        <!-- <br> -->
+                        <div class="col-sm-12 question-show-answer">
+                            <div>{!! nl2br(e($question->answer)) !!}</div>
+                        </div>
+               
+                       @if($question->sup_image)
+                            <div class="col-sm-12">
+                                <img src="{{ Storage::url($question->sup_image) }}" width="25%">
+                            </div>
+                       @endif
+                    </div>
                 </div>
                 </div>
             </div>
