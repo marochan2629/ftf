834843ae1352790b2c949cf681addb02a81a8432
marochan <iwata26th0776heartasone@gmail.com>
2022-08-02 10:45:55 +0900

    質問作成画面のレイアウト修正、勢い余ってトップページにアニメーションを付与

--- resources/views/app/home.blade.php
+++ resources/views/app/home.blade.php
@@ -1,8 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<!-- <div class="content"> -->
-    <div class="top-page">
+    <div class="top-page" id="top-page">
         <div class="hero-area">
             <div class="hero-images">
                 <img src="/images/berlin-cathedral-g76d15908b_1920.jpg" class="slideshow hero-image">
@@ -12,39 +11,64 @@
             </div>
             
             <div class="main">
-                <h1 class="catchphrase">宗教を知る<br>宗教に向き合う</h1>
-                <!-- <h1>宗教に向き合う</h1> -->
-                <a href="#" class="title">Face to Faith</a>
+                <h1 class="catchphrase catchphrase1" id="catchphrase1">宗教を知る</h1>
+                <h1 class="catchphrase catchphrase2" id="catchphrase2">宗教に向き合う</h1>
+                <a href="#" class="title" id="title">Face to Faith</a>
             </div>
         </div>
+
         <div class="concept">
             <h1>宗教をもっと身近に、もっとわかりやすく</h1>
             <p>普段身近に触れることがない宗教のことを、気軽に学べるサイトです。<br>本当はもっとなんか書いた方がいいけど思いつかんので適当に。</p>
         </div>
+
         <div class="card-deck services">
             <div class="card service service1">
                 <div class="card-body">
                     <a href="/article/index"><img src="../images/leaves-gb0c9b754b_1920.jpg" class="service-img"><p class="service-name">読む<br>LEARN</p></a>
-                    
-                    <!-- <p>LEARN</p> -->
                 </div>
             </div>
+
             <div class="card service service2">
                 <div class="card-body">
                     <a href="/question/index"><img src="../images/grandmother-g1fa961d0c_1920.jpg" class="service-img"><p class="service-name">聞く<br>QUESTIONS</p></a>
-                    
                 </div>
             </div>
+
             <div class="card service service3">
                 <div class="card-body">
                     <a href="/photo/index"><img src="../images/zhangjiajie-g66b1d36f8_1920.jpg" class="service-img"><p class="service-name">見る<br>PHOTOS</p></a>
-                    
                 </div>
             </div>
         </div>
+
         <div class="notifications">
             <h1>お知らせ機能作ってここに表示させようかなあ。もっと上の方がいいかな</h1>
         </div>
     </div>
-
 @endsection
+
+<script>
+    window.addEventListener('DOMContentLoaded', function(){
+        // 一旦各要素を透明にする
+        $("#catchphrase1").css({opacity:'0'});
+        $("#catchphrase2").css({opacity:'0'});
+        $("#title").css({opacity:'0'});
+
+        // 最初の要素は画面を開いた直後にフェードイン
+        setTimeout(function(){
+            $("#catchphrase1").stop().animate({opacity:'1'},1500);
+        }, 0)
+
+        // 次の要素は少し後からフェードイン
+        setTimeout(function(){
+            $("#catchphrase2").stop().animate({opacity:'1'},1500);
+        }, 1200)
+
+        // 最後も同様に
+        setTimeout(function(){
+            $("#title").stop().animate({opacity:'1'},1500);
+        }, 2400)
+        
+    });
+</script>
--- resources/views/app/question/create.blade.php
+++ resources/views/app/question/create.blade.php
@@ -1,44 +1,46 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="text-center mt-2 mb-5">質問画面</h1>
-    <div class="container mb-5">
-        <form action="{{ route('question.store') }}" method="POST"  enctype="multipart/form-data">
-            @csrf
-            <div class="form-group row">
-                <p class="col-sm-1 col-form-label">タイトル</p>
-                <div class="col-sm-11">
-                    <input type="text" name="title" class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}" value="{{ old('title') }}">
-                    @if ($errors->has('title'))
-                        <div class="invalid-feedback">
-                        {{ $errors->first('title') }}
-                        </div>
-                    @endif
+    <div class="question-create">
+        <h1 class="text-center mt-2">質問投稿</h1>
+        <div class="container">
+            <form action="{{ route('question.store') }}" method="POST"  enctype="multipart/form-data">
+                @csrf
+                <div class="form-group row">
+                    <p class="col-sm-1 col-form-label">タイトル</p>
+                    <div class="col-sm-11">
+                        <input type="text" name="title" class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}" value="{{ old('title') }}">
+                        @if ($errors->has('title'))
+                            <div class="invalid-feedback">
+                            {{ $errors->first('title') }}
+                            </div>
+                        @endif
+                    </div>
                 </div>
-            </div>
-            
-            <div class="form-group row">
-                <p class="col-sm-1 col-form-label">本文</p>
-                <div class="col-sm-11">
-                    <textarea class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}" name="body" id="body" cols="100" rows="15">{{ old('body') }}</textarea>
-                    @if ($errors->has('body'))
-                        <div class="invalid-feedback">
-                            {{ $errors->first('body') }}
-                        </div>
-                    @endif
+                
+                <div class="form-group row">
+                    <p class="col-sm-1 col-form-label">本文</p>
+                    <div class="col-sm-11">
+                        <textarea class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}" name="body" id="body" cols="100" rows="15">{{ old('body') }}</textarea>
+                        @if ($errors->has('body'))
+                            <div class="invalid-feedback">
+                                {{ $errors->first('body') }}
+                            </div>
+                        @endif
+                    </div>
                 </div>
-            </div>
-
-            <div class="form-group row">
-                <p class="col-sm-1 col-form-label">画像</p>
-                <div class="col-sm-11">
-                    <input type="file" name="image">
+    
+                <div class="form-group row">
+                    <p class="col-sm-1 col-form-label">画像</p>
+                    <div class="col-sm-11">
+                        <input type="file" name="image">
+                    </div>
                 </div>
-            </div>
-
-            <div class="text-center">
-                <button type="submit">記事を投稿する</button>
-            </div>
-        </form>
+    
+                <div class="text-center">
+                    <button type="submit">記事を投稿する</button>
+                </div>
+            </form>
+        </div>
     </div>
 @endsection
