35242c91b90f67fe6b19da8e5b57e4da60a18a90
marochan <iwata26th0776heartasone@gmail.com>
2022-10-23 13:50:24 +0900

    質問詳細画面SP対応

--- resources/views/app/question/show.blade.php
+++ resources/views/app/question/show.blade.php
@@ -6,23 +6,23 @@
             <div class="question-show-outer">
                 <div class="question-show-inner">
                     <div class="question-show-question-wrapper">
-                        <div class="col-sm-12 question-show-title">
+                        <div class="question-show-title">
                             <p>{{ $question->title }}</p>
                         </div>
 
-                        <div class="col-sm-12 question-show-body">
+                        <div class="question-show-body">
                             <p>{!! nl2br(e($question->body)) !!}</p>
                         </div>
                     </div>
                     
                     <div class="question-show-answer-wrapper">
                         <h2>Answer</h2>
-                        <div class="col-sm-12 question-show-answer">
+                        <div class="question-show-answer">
                             <div>{!! nl2br(e($question->answer)) !!}</div>
                         </div>
                
                        @if($question->sup_image)
-                            <div class="col-sm-12 question-show-sup_image">
+                            <div class="question-show-sup_image">
                                 <a href="{{ Storage::url($question->sup_image) }}" rel="lightbox">
                                     <img src="{{ Storage::url($question->sup_image) }}" width="25%">
                                 </a>
