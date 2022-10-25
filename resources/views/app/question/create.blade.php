53897591322d816175831834b057e57658ccf4e1
marochan <iwata26th0776heartasone@gmail.com>
2022-10-25 16:07:18 +0900

    質問投稿画面SP対応

--- resources/views/app/question/create.blade.php
+++ resources/views/app/question/create.blade.php
@@ -4,14 +4,13 @@
     <div class="question-show">
         <div class="container mb-5">
             <div class="question-show-outer">
-                <div class="question-show-inner">
-                    <div class="container my-5">
-                        <h1 class="text-center mb-5">質問投稿</h1>
+                <div class="question-show-inner py-5">
+                        <h1 class="text-center">質問投稿</h1>
                         <form action="{{ route('question.store') }}" method="POST"  enctype="multipart/form-data">
                             @csrf
                             <div class="form-group row">
-                                <p class="col-sm-2 col-form-label">タイトル</p>
-                                <div class="col-sm-10">
+                                <p class="col-lg-2 col-12">タイトル</p>
+                                <div class="col-lg-10 col-12">
                                     <input type="text" name="title" class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}" value="{{ old('title') }}">
                                     @if ($errors->has('title'))
                                         <div class="invalid-feedback">
@@ -22,8 +21,8 @@
                             </div>
                             
                             <div class="form-group row">
-                                <p class="col-sm-2 col-form-label">本文</p>
-                                <div class="col-sm-10">
+                                <p class="col-lg-2 col-12">本文</p>
+                                <div class="col-lg-10 col-12">
                                     <textarea class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}" name="body" id="body" cols="100" rows="15">{{ old('body') }}</textarea>
                                     @if ($errors->has('body'))
                                         <div class="invalid-feedback">
@@ -34,8 +33,8 @@
                             </div>
                 
                             <div class="form-group row">
-                                <p class="col-sm-2 col-form-label">画像</p>
-                                <div class="col-sm-10">
+                                <p class="col-lg-2 col-12">画像</p>
+                                <div class="col-lg-10 col-12">
                                     <input type="file" name="image">
                                 </div>
                             </div>
@@ -44,7 +43,6 @@
                                 <button type="submit">記事を投稿する</button>
                             </div>
                         </form>
-                    </div>
                 </div>
                 </div>
             </div>
